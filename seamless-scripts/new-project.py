8e267e8d6a3d8f9035256609469d99860b3858ad
Sjoerd de Vries <sjdv1982@gmail.com>
2024-09-28 01:23:06 +0200

    update new-project.py to 0.13

--- seamless-scripts/new-project.py
+++ seamless-scripts/new-project.py
@@ -1,9 +1,15 @@
 import sys, os, shutil, json
-from seamless.highlevel import Context
-import argparse
-import json
+import seamless
 
-parser = argparse.ArgumentParser()
+os.environ["SEAMLESS_SILENT"] = "1"
+seamless.delegate(False)
+from seamless.workflow.highlevel import Context
+import argparse
+
+parser = argparse.ArgumentParser(
+    prog="seamless-new-project",
+    description="Create a new Seamless webserver project inside the current directory",
+)
 parser.add_argument(
     "project_name",
     help="Name of the project",
@@ -11,8 +17,10 @@ parser.add_argument(
 args = parser.parse_args()
 project_name = args.project_name
 
+
 def pr(*args):
     print(*args, file=sys.stderr)
+
 
 notebook = r"""{
  "cells": [
@@ -34,11 +42,7 @@ notebook = r"""{
  "nbformat_minor": 2
 }"""
 
-subpaths = [
-    "web",
-    "graph",
-    "vault"
-]
+subpaths = ["web", "graph", "vault"]
 for subpath in subpaths:
     if os.path.exists(subpath):
         pr("%s/ already exists, aborting..." % subpath)
@@ -47,7 +51,6 @@ for subpath in subpaths:
 for subpath in subpaths:
     os.mkdir(subpath)
 
-import seamless
 seamless_dir = os.path.dirname(seamless.__file__)
 
 empty = Context()
@@ -55,13 +58,13 @@ empty.save_graph("graph/{0}.seamless".format(project_name))
 del empty
 
 f = "webgen.seamless"
-source = os.path.join(seamless_dir, "graphs", f)
+source = os.path.join(seamless_dir, "workflow", "graphs", f)
 dest = os.path.join("graph", "{0}-webctx.seamless".format(project_name))
 shutil.copy(source, dest)
 graph = json.load(open(dest))
 
 ctx = Context()
-ctx.add_zip(os.path.join(seamless_dir, "graphs", "webgen.zip"))
+ctx.add_zip(os.path.join(seamless_dir, "workflow", "graphs", "webgen.zip"))
 ctx.set_graph(graph)
 ctx.save_vault("vault")
 
@@ -93,20 +96,63 @@ else:
         f.write(gitignore)
 
 code = '''
-PROJNAME = "%s"
+PROJNAME = "{project_name}"
+
+DELEGATION_LEVEL = 0
+
+"""
+Change DELEGATION_LEVEL to the appropriate level:
+
+DELEGATION_LEVEL = 0
+Runs load_vault on project startup
+Runs save_vault on project save.
+All input/output buffers and results are held in memory.
+
+DELEGATION_LEVEL = 1
+External read buffer servers/folders may be configured. 
+On project startup, run load_vault if vault/ exists.
+On project save, run save_vault if vault/ exists
+
+DELEGATION_LEVEL = 2
+Buffers are stored in an external buffer server. 
+Such a server can be launched using "seamless-delegate none"
+Vaults are being ignored on project load/save. You are recommended to upload the vault/ directory 
+using "seamless-upload"
+
+DELEGATION_LEVEL = 3
+Buffers are stored in an external buffer server.
+Results are stored as checksums in a database server.
+Such servers can be launched using "seamless-delegate none"
+Vaults are being ignored on project load/save. You are recommended to upload the vault/ directory 
+using "seamless-upload"
+
+DELEGATION_LEVEL = 4
+All jobs, buffers and results are delegated to an external assistant
+Such an assistant can be launched using "seamless-delegate <name of assistant>" 
+Vaults are being ignored on project load/save. You are recommended to upload the vault/ directory 
+using "seamless-upload"
+"""
 
 import os, sys, shutil
+import seamless, seamless.config
+from seamless.workflow.highlevel import (Context, Cell, Transformer, Module, Macro, 
+                                SimpleDeepCell, FolderCell, DeepCell, DeepFolderCell)
 
-import seamless
+def pr(*args):
+    print(*args, file=sys.stderr)
 
-from seamless.highlevel import Context, Cell, Transformer, Module, Macro
+_curr_delegation_level = seamless.config.get_delegation_level()
+if _curr_delegation_level is None:
+    seamless.delegate(DELEGATION_LEVEL)
+elif int(_curr_delegation_level) != DELEGATION_LEVEL:
+    pr("DELEGATION_LEVEL overridden to {{}} by previous seamless.delegate() call".format(_curr_delegation_level))
+    DELEGATION_LEVEL = int(_curr_delegation_level)
+
 
 ctx = None
 webctx = None
 save = None
-
-def pr(*args):
-    print(*args, file=sys.stderr)
+export = None
 
 async def define_graph(ctx):
     """Code to define the graph
@@ -114,60 +160,19 @@ async def define_graph(ctx):
     """
     pass
 
-def load_database():
-    # To connect to a Seamless database, specify the following environment variables:
-    # SEAMLESS_DATABASE_IP, SEAMLESS_DATABASE_PORT
-    #
-    # They are passed into the Seamless Docker container when you run 
-    #  seamless-load-project, seamless-jupyter, etc.
-    # Seamless provides default values for these environment variables
-    # These defaults will connect to the database started with seamless-database command.
-    # 
-    # Then, uncomment the following lines:
-    # To read buffers and transformation results from the database:
-    #
-    # seamless.database_cache.connect()  
-    #
-    # To write buffers and transformation results into the database:
-    #
-    # seamless.database_sink.connect()
-    return
-
-COMMUNION_MSG=""
-async def load_communion():
-    global COMMUNION_MSG
-    # To connect to a Seamless communion peer, such as jobless or a jobslave,
-    # specify the following environment variables:
-    # SEAMLESS_COMMUNION_IP
-    # SEAMLESS_COMMUNION_PORT
-    # or: SEAMLESS_COMMUNION_INCOMING (comma-separated list of multiple peers, as IP:port,IP:port,...)
-    #
-    # These are passed into the Seamless Docker container when you run 
-    #  seamless-load-project, seamless-jupyter, etc.
-    # If these environment variables are not defined, 
-    # Seamless provides default values for them.
-    # These default values will try to connect to jobless.
-    # 
-    # Then, uncomment the following lines:
-    #
-    # seamless.communion_server.configure_master({
-    #     "buffer": True,
-    #     "buffer_status": True,
-    #     "buffer_info": True,
-    #     "transformation_job": True,
-    #     "transformation_status": True,
-    #     "semantic_to_syntactic": True,
-    # })
-    # await seamless.communion_server.start_async()
-    # npeers = len(seamless.communion_server.peers)
-    # COMMUNION_MSG="\\n\\n{} communion peer(s) found.".format(npeers)
-    return
+def load_ipython():
+    import asyncio
+    import seamless.workflow
+    loop = seamless.workflow._original_event_loop
+    asyncio.set_event_loop(loop)
+    coro = load()
+    loop.run_until_complete(coro)
 
 async def load():
-    from seamless.metalevel.bind_status_graph import bind_status_graph_async
+    from seamless.workflow.metalevel.bind_status_graph import bind_status_graph_async
     import json
 
-    global ctx, webctx, save
+    global ctx, webctx, save, export
 
     try:
         ctx
@@ -175,10 +180,8 @@ async def load():
         pass
     else:
         if ctx is not None:
-            pr('"ctx" already exists. To reload, do "ctx = None" or "del ctx" before load()')
+            pr('"ctx" already exists. To reload, do "ctx = None" or "del ctx" before "await load()"')
             return
-    load_database()    
-    await load_communion()
 
     for f in (
         "web/index-CONFLICT.html",
@@ -186,30 +189,30 @@ async def load():
         "web/webform-CONFLICT.txt",
     ):
         if os.path.exists(f):
-            if open(f).read().rstrip("\\n ") in ("", "No conflict"):
+            try:
+                if open(f).read().rstrip("\\n ") in ("", "No conflict"):
+                    continue
+            except UnicodeDecodeError:
                 continue
             dest = f + "-BAK"
             if os.path.exists(dest):
                 os.remove(dest)            
-            pr("Existing '{}' found, moving to '{}'".format(f, dest))
+            pr("Existing '{{}}' found, moving to '{{}}'".format(f, dest))
             shutil.move(f, dest)
     ctx = Context()
     empty_graph = ctx.get_graph()
-    try:
-        seamless._defining_graph = True
-        await define_graph(ctx)
-    finally:
-        try:
-            del seamless._defining_graph
-        except AttributeError:
-            pass
+    await define_graph(ctx)
     new_graph = ctx.get_graph()
     graph_file = "graph/" + PROJNAME + ".seamless"
-    ctx.load_vault("vault")
+    if DELEGATION_LEVEL == 0: 
+        ctx.load_vault("vault")
+    elif DELEGATION_LEVEL == 1:
+        if os.path.exists("vault"):
+            ctx.load_vault("vault")
     if new_graph != empty_graph:
-        pr("*** define_graph() function detected. Not loading '{}'***\\n".format(graph_file))
+        pr("*** define_graph() function detected. Not loading '{{}}'***\\n".format(graph_file))
     else:
-        pr("*** define_graph() function is empty. Loading '{}' ***\\n".format(graph_file))
+        pr("*** define_graph() function is empty. Loading '{{}}' ***\\n".format(graph_file))
         graph = json.load(open(graph_file))        
         ctx.set_graph(graph, mounts=True, shares=True)
         await ctx.translation(force=True)
@@ -221,6 +224,7 @@ async def load():
         mounts=True,
         shares=True
     )
+    
     def save():
         import os, itertools, shutil
 
@@ -229,28 +233,56 @@ async def load():
                 return filename
             for n in itertools.count():
                 n2 = n if n else ""
-                new_filename = "{}.bak{}".format(filename, n2)
+                new_filename = "{{}}.bak{{}}".format(filename, n2)
                 if not os.path.exists(new_filename):
                     break
             shutil.move(filename, new_filename)
             return filename
 
+        try:
+            ctx.translate()
+        except Exception:
+            pass
         ctx.save_graph(backup("graph/" + PROJNAME + ".seamless"))
+        try:
+            webctx.translate()
+        except Exception:
+            pass        
         webctx.save_graph(backup("graph/" + PROJNAME + "-webctx.seamless"))
-        ctx.save_vault("vault")
-        webctx.save_vault("vault")
+        if DELEGATION_LEVEL == 0: 
+            ctx.save_vault("vault")
+            webctx.save_vault("vault")
+        elif DELEGATION_LEVEL == 1:
+            if os.path.exists("vault"):
+                ctx.save_vault("vault")
+                webctx.save_vault("vault")
 
-    pr("""Project loaded.
+    def export():
+        filename = "graph/{project_name}.zip"
+        ctx.save_zip(filename)
+        pr(f"{{filename}} saved")
+        filename = "graph/{project_name}-webctx.zip"
+        webctx.save_zip(filename)
+        pr(f"{{filename}} saved")
+
+    await ctx.translation(force=True)
+    await ctx.translation(force=True)
+    
+    _rest_port = seamless.workflow.shareserver.shareserver.rest_port
+    pr(f"""Project loaded.
 
     Main context is "ctx"
     Web/status context is "webctx"
 
-    Open http://localhost:<REST server port> to see the web page
-    Open http://localhost:<REST server port>/status/status.html to see the status
+    Open http://localhost:{{_rest_port}} to see the web page
+    Open http://localhost:{{_rest_port}}/status/status.html to see the status
 
-    Run save() to save the project{}
-    """.format(COMMUNION_MSG))
-''' % (project_name,)
+    Run save() to save the project workflow file.
+    Run export() to generate zip files for web deployment.
+    """)
+'''.format(
+    project_name=project_name
+)
 
 with open("load-project.py", "w") as f:
     f.write(code)
@@ -258,7 +290,8 @@ with open("load-project.py", "w") as f:
 with open("{0}.ipynb".format(project_name), "w") as f:
     f.write(notebook)
 
-pr("""Project {0} created.
+pr(
+    """Project {0} created.
 
 - Use seamless-load-project to start up IPython
 or:
@@ -266,10 +299,13 @@ or:
   and in the Jupyter browser window,
   open /home/jovyan/cwd/{0}.ipynb
 
-If Seamless needs to execute Docker transformers:
-- Use seamless-load-project-trusted to start up IPython
+If Seamless does not need to execute Docker transformers:
+- Use seamless-load-project-safe to start up IPython
 or:
-- Use seamless-jupyter-trusted to start up Jupyter
+- Use seamless-jupyter-safe to start up Jupyter
   and in the Jupyter browser window,
   open /home/jovyan/cwd/{0}.ipynb
-""".format(project_name))
+""".format(
+        project_name
+    )
+)
